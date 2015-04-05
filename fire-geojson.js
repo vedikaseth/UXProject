7b9923b1dfe4d03163ffcd57071877528023f33a
Ronak Nisher <nisher.ronak@gmail.com>
2015-04-05 16:08:56 -0400

    Basic Map with hover and click functionality

---
+++ fire-geojson.js
@@ -0,0 +1,103 @@
+var fireLoc = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -78.57604104099966,
+                    35.79644938500047
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Fire Station A"
+            },
+            "id": 51
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -78.68478087099965,
+                    35.816213019000486
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Fire Station B"
+            },
+            "id": 52
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -78.56734429499966,
+                    35.75327112600047
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Fire Station C"
+            },
+            "id": 54
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9960754,
+                    39.7498956
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 55
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9933717,
+                    39.7477264
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 57
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9913392,
+                    39.7432392
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 58
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9788452,
+                    39.6933755
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 74
+        }
+    ]
+};
